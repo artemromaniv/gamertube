981a01c5c827648fd3e75c76559539b65e9753c9
Artem <artemromaniv@yahoo.com>
2022-11-09 12:42:13 +0000

    minor css changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,15 +15,15 @@ const RootLayout = ({ children }: Props) => {
         <meta name="viewport" content="device-width, initial-scale=1.0" />
       </head>
       <body>
-        <header className="header">
-          <Link href={"/"}>
-            <Image src={LogoText} alt="" className="logo" />
-          </Link>
-          <div className="search_desktop">
-            <Search />
-          </div>
-        </header>
-        <ReactQueryWrapper>{children}</ReactQueryWrapper>
+          <header className="header">
+            <Link href={"/"}>
+              <Image src={LogoText} alt="" className="logo" />
+            </Link>
+            <div className="search_desktop">
+              <Search />
+            </div>
+          </header>
+          <ReactQueryWrapper>{children}</ReactQueryWrapper>
       </body>
     </html>
   );
