bbc512d918e270b9b42a04aee0506c0832094def
Artem <artemromaniv@yahoo.com>
2022-11-09 11:52:18 +0000

    make logo link to homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,8 @@ import "../styles/globals.css";
 import Image from "next/image";
 import ReactQueryWrapper from "./ReactQueryWrapper";
 import LogoText from "../assets/LogoText.svg";
-import Search from '../components/Search'
+import Search from "../components/Search";
+import Link from "next/link";
 type Props = {
   children: React.ReactNode;
 };
@@ -14,9 +15,11 @@ const RootLayout = ({ children }: Props) => {
         <meta name="viewport" content="device-width, initial-scale=1.0" />
       </head>
       <body>
-        <header className='header'>
-          <Image src={LogoText} alt="" className='logo' />
-          <div className='search_desktop'>
+        <header className="header">
+          <Link href={"/"}>
+            <Image src={LogoText} alt="" className="logo" />
+          </Link>
+          <div className="search_desktop">
             <Search />
           </div>
         </header>
