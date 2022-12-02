227a1788ac91a3aa147766695cc886a856294f7a
Artem <artemromaniv@yahoo.com>
2022-12-02 15:49:33 +0000

    create content grid in New and Trending section

--- components/BrowseSection.tsx
+++ components/BrowseSection.tsx
@@ -1,16 +1,35 @@
 import Link from "next/link";
 import styles from "../styles/Browse.module.css";
 import { Award } from "react-feather";
+import Ghostsong from "../assets/GhostSong.jpg";
+import HollowKnight from "../assets/HollowKnight.jpg";
 const BrowseSection = () => {
   return (
     <section className={styles.container}>
-      <Link href={"/best"} className={styles.card}>
+      <Link
+        href={"/best"}
+        className={styles.card}
+        style={{
+          backgroundImage: `url(${Ghostsong.src})`,
+          width: "100%",
+          height: "100%",
+          objectFit: "contain",
+        }}
+      >
         <div className={styles.card_items}>
-          <Award width={45} height = {45} />
+          <Award width={45} height={45} />
           <span>BEST</span>
         </div>
       </Link>
-      <Link href={"/tags"} className={styles.card}>
+      <Link
+        href={"/tags"}
+        className={styles.card}
+        style={{
+          backgroundImage: `url(${HollowKnight.src})`,
+          maxWidth: "100%",
+          objectFit: "none",
+        }}
+      >
         <span>TAGS</span>
       </Link>
       <Link href={"/new"} className={styles.card}>
--- components/NewAndTrending.tsx
+++ components/NewAndTrending.tsx
@@ -1,10 +1,20 @@
 import Starfield from "../assets/Starfield.jpg";
 import Image from "next/image";
 import styles from "../styles/NewAndTrending.module.css";
+import { useQuery } from "@tanstack/react-query";
+
 const NewAndTrending = () => {
+
+  const getGamesData = async () => {
+      const res = await fetch(`https://api.rawg.io/api/games?key=${process.env.API_KEY}`);
+      return res.json
+  }
+
+  const {data, error, isLoading} = useQuery({queryKey:['gamesData'],queryFn:getGamesData});
+
   return (
     <section className={styles.container}>
-      <div>
+      {/* <div>
         <Image src={Starfield} alt="starfield" className={styles.image} />
       </div>
       <div>
@@ -12,7 +22,11 @@ const NewAndTrending = () => {
       </div>
       <div>
         <Image src={Starfield} alt="starfield" className={styles.image}/>
-      </div>
+      </div> */}
+
+      <pre>
+        {JSON.stringify(isLoading,null,4)}
+      </pre>
     </section>
   );
 };
