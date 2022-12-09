52f1f16d9b16b8809932e5279e945c19381e85d5
Artem <artemromaniv@yahoo.com>
2022-12-09 15:09:48 +0000

    fetch Data from game database

--- components/NewAndTrending.tsx
+++ components/NewAndTrending.tsx
@@ -1,34 +1,14 @@
-import Starfield from "../assets/Starfield.jpg";
 import Image from "next/image";
 import styles from "../styles/NewAndTrending.module.css";
 import { useQuery } from "@tanstack/react-query";
+import { fetchGamesData } from "../api/fetchGamesData";
 
 const NewAndTrending = () => {
-
-  const getGamesData = async () => {
-      const res = await fetch(`https://api.rawg.io/api/games?key=${process.env.API_KEY}`);
-      return res.json
-  }
-
-  const {data, error, isLoading} = useQuery({queryKey:['gamesData'],queryFn:getGamesData});
-
-  return (
-    <section className={styles.container}>
-      {/* <div>
-        <Image src={Starfield} alt="starfield" className={styles.image} />
-      </div>
-      <div>
-        <Image src={Starfield} alt="starfield" className={styles.image}/>
-      </div>
-      <div>
-        <Image src={Starfield} alt="starfield" className={styles.image}/>
-      </div> */}
-
-      <pre>
-        {JSON.stringify(isLoading,null,4)}
-      </pre>
-    </section>
+  const { data, isLoading, error } = useQuery(["trending"], () =>
+    fetchGamesData("games")
   );
+  !error ? console.log(data) : console.log(error);
+  return <section className={styles.container}></section>;
 };
 
 export default NewAndTrending;
