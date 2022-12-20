5237be8df457757218c797bb8bd1d75f5466b0e7
Artem <artemromaniv@yahoo.com>
2022-12-20 12:58:44 +0000

    fetching details of the game using it's ID as a query

---
+++ api/fetchGameDetails.ts
@@ -0,0 +1,11 @@
+export const fetchGameDetails = async (id:string | number) => {
+    const BASE_URL = "https://api.rawg.io/api";
+    const RAWG_API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+    const response = await fetch(`${BASE_URL}/games/${id}?key=${RAWG_API_KEY}`);
+    if (!response.ok) {
+      throw new Error("Failed to fetch game data");
+    }
+    const data = await response.json();
+    return data;
+  };
+  
--- app/[gameId]/page.tsx
+++ app/[gameId]/page.tsx
@@ -1,14 +1,25 @@
-import React from 'react'
+"use client";
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import { fetchGameDetails } from "../../api/fetchGameDetails";
 type PageProps = {
   params: {
-    gameId:number
-  }
-}
+    gameId: number;
+  };
+};
 
-const page = (props:PageProps) => {
+const page = (props: PageProps) => {
+  const { data } = useQuery(
+    [props.params.gameId],
+    () => fetchGameDetails(props.params.gameId),
+    { staleTime: Infinity }
+  );
+
   return (
-    <div>Game id: {props.params.gameId}</div>
-  )
-}
+    <div>
+      <pre>{JSON.stringify(data, null, 4)}</pre>
+    </div>
+  );
+};
 
-export default page
+export default page;
